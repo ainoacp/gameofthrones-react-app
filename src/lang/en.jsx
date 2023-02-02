46e4ce7492a26127d3b36216927e71a95a9c2884
Nerea <nerea.acbs@gmail.com>
2023-02-02 18:16:13 +0100

    pagina home y lenguajes

---
+++ src/lang/en.jsx
@@ -0,0 +1,8 @@
+export const en = {
+    "lang_es": "spanish",
+    "lang_en": "english",
+    "characters": "characters",
+    "cronology": "cronology",
+    "houses": "houses",
+    
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,22 @@
 import NavComponent from "../../components/shared/NavComponent/NavComponent";
+import TranslatorNavComponent from "../../components/shared/TranslatorNavComponent/TranslatorNavComponent";
+import "./HomePage.css"
 
 export default function HomePage(){
-    return <NavComponent/>
+
+
+    return (
+        <>
+        <div className="container">
+            <TranslatorNavComponent className="translator-nav"></TranslatorNavComponent>
+        
+        
+            <h1 className="title">GAMES OF TRONES</h1>
+        
+            <div className="nav-component">
+                <NavComponent/>
+            </div>
+        </div>
+        </>
+    )
 }
