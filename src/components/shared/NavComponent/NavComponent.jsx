c0c507d2d3acd6c0a8a13a4a7845e6eec8d35128
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-05 15:24:14 +0000

    .

--- src/components/shared/NavComponent/NavComponent.jsx
+++ src/components/shared/NavComponent/NavComponent.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 
+
 export default function NavComponent(){
     return (
     <div>
-        <Link to="/characters">Personajes</Link>
-        <Link to="/houses">Casas</Link>
-        <Link to="/cronology">Cronologia</Link>
+        <Link to="/characters" className="c-nav-link">Personajes</Link>
+        <Link to="/houses" className="c-nav-link">Casas</Link>
+        <Link to="/cronology" className="c-nav-link">Cronologia</Link>
     </div>
     )
 }
