4961e18e5f86c1d29fe0f252aa731c9acfda0fa8
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-07 16:02:25 +0000

    noImage fixed

--- src/components/shared/GalleryCharacters/GalleryCharacters.jsx
+++ src/components/shared/GalleryCharacters/GalleryCharacters.jsx
@@ -9,7 +9,7 @@ export default function GalleryCharacters({characters}) {
             <div className="gallery">
             {characters.map((character) => (
             <figure character={character} key={character._id} className="card">
-                <img src={character.image} alt={character.name}/>
+                <img src={character.image || 'https://i.pinimg.com/736x/1f/26/0e/1f260eab90fab5cb5df68937981a8994.jpg'} alt={character.name}/>
                 <figcaption>
                     <Link to={`/characters/${character.name}`}>{character.name}</Link>
                 </figcaption>
--- src/pages/CharactersPage/CharactersPage.jsx
+++ src/pages/CharactersPage/CharactersPage.jsx
@@ -12,14 +12,31 @@ export default function CharactersPage(){
 
     const [characters, setCharacters] = useState([])
     const [filteredCharacters, setFilterCharacters] = useState([])
-    
+
+    const imagesUrlsFail = [
+        'https://vignette.wikia.nocookie.net/gameofthrones/images/a/a8/Davos_s8_ep2_infobox.jpg/revision/latest/scale-to-width-down/319?cb=20190422024848',
+        'https://vignette.wikia.nocookie.net/gameofthrones/images/9/96/Oberyn-Martell-house-martell-37118334-2832-4256.jpg/revision/latest/scale-to-width-down/333?cb=20150815065729',
+        'https://vignette.wikia.nocookie.net/gameofthrones/images/6/63/GENDRYAKOFTSK.PNG/revision/latest?cb=20190422030504',
+        'https://vignette.wikia.nocookie.net/gameofthrones/images/1/1b/Grenn.jpg/revision/latest?cb=20180702193920',
+        'https://vignette.wikia.nocookie.net/gameofthrones/images/a/af/Mossador-s5e1-v2.jpg.jpg/revision/latest/scale-to-width-down/265?cb=20150427224509',
+        'https://vignette.wikia.nocookie.net/gameofthrones/images/e/eb/S06E05_-_Kinvara_%281%29.jpg/revision/latest/scale-to-width-down/339?cb=20160812222946'
+    ]
 
     const getCharacters = async () => {
         const res = await axios.get(`https://api.got.show/api/show/characters/`);
         console.log(res);
-        setCharacters(res.data);
-        setFilterCharacters(res.data);
+        let fixedCharacter = res.data.map((character) => {
+            if (imagesUrlsFail.includes(character?.image)) {
+                console.log(character.image)
+                character.image = null; 
+                return character;
+            } 
+            return character;
+        })
+        setCharacters(fixedCharacter);
+        setFilterCharacters(fixedCharacter);
     }
+
     const filterCharacters = async (searchText) => {
         let newCharacters = characters.filter((character) => character.name.toLowerCase().includes(searchText.toLowerCase()))
         setFilterCharacters(newCharacters);
@@ -29,7 +46,7 @@ export default function CharactersPage(){
         <div className="c-characters-page">
             <div className="c-characters-header">
                 <div className="c-characters-form-container">
-                    <Searcher onSubmit={filterCharacters}/>
+                    <Searcher search={filterCharacters}/>
                 </div>
                 <div className="c-characters-buttons">
                     <HomeLink/>
