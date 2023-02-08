53860484fd822fa4f1a20c7c4612c7ad1ee48b5d
ainoacp <118761900+ainoacp@users.noreply.github.com>
2023-02-08 09:31:41 +0000

    updated sort component

--- src/components/shared/Sorter/Sorter.jsx
+++ src/components/shared/Sorter/Sorter.jsx
@@ -13,8 +13,7 @@ export default function Sorter({sort, setSort, age }) {
     return (
         <>
             <div className='button-container'>
-                {sort==='Ascending' && <button onClick={sorting} className='circle'>{age}</button>}
-                {sort==='Descending' && <button onClick={sorting} className='circle'>{age}</button>}
+                <button onClick={sorting} className='circle'>{age}</button>
                 <div className='line'>
                     {sort==='Ascending' && <div className='flecha'>{'>'}</div>}
                     {sort==='Descending' && <div className='flecha'>{'<'}</div>}
